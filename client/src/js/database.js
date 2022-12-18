475281ba883cca921fd818530528e3b693a9e019
Ryan <ryanpjhickey@gmail.com>
2022-12-18 09:26:07 -0800

    commit changes to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const progressiveDB = await openDB('jate', 1);
+  const text = progressiveDB.transaction('jate', 'readwrite');
+  const storeObject = text.objectStore('jate');
+  const request = storeObject.put({ id: 1, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const progerssiveDB = await openDB('jate', 1);
+  const text = progerssiveDB.transaction('jate', 'readonly');
+  const getObject = text.objectStore('jate');
+  const request = getObject.get(1);
+  const result = await request;
+  return result?.value;
+};
 
 initdb();
